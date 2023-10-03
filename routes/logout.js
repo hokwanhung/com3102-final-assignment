b04bc713051e398c726608e490c4a7a8c056ff5d
khho <hokwanhung.2019@gmail.com>
2023-10-03 16:07:21 +0800

    fix: website logout function due to recent changes in passport.js

--- routes/logout.js
+++ routes/logout.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const methodOverride = require('method-override');
+
 router.use(methodOverride('_method'));
 
 // allows the delete method being override by form posting from log out button
-router.delete('/', (req, res) => {
-    req.logOut();  // remove the user object from session.passport property
+router.delete('/', (req, res, next) => {
+    req.logOut(function(err) {
+        if (err) {
+            return next(err);
+        }
+    });  // remove the user object from session.passport property
     res.redirect('/');
 });
 
